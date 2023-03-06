69fa893f0e38bebfb0d65097725273bf7ed87a3c
Mihir <mp06121@gmail.com>
2023-03-06 10:51:05 +0530

    routes-completed

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,4 +1,15 @@
 import Footer from "../components/Footer"
+import proj_1_1 from "../assets/proj_1/proj_1_1.jpg"
+import proj_1_2 from "../assets/proj_1/proj_1_2.jpg"
+import proj_1_3 from "../assets/proj_1/proj_1_3.jpg"
+import proj_1_4 from "../assets/proj_1/proj_1_4.jpg"
+import proj_1_5 from "../assets/proj_1/proj_1_5.jpg"
+import proj_1_6 from "../assets/proj_1/proj_1_6.jpg"
+import proj_1_7 from "../assets/proj_1/proj_1_7.jpg"
+import proj_1_8 from "../assets/proj_1/proj_1_8.jpg"
+import proj_1_9 from "../assets/proj_1/proj_1_9.jpg"
+import proj_1_10 from "../assets/proj_1/proj_1_10.jpg"
+import proj_1_11 from "../assets/proj_1/proj_1_11.jpg"
 
 const Gallery = () => {
     return (
@@ -12,16 +23,83 @@ const Gallery = () => {
                     </div>
                 </div>
             </div>
+            <div className="py-3 py-md-5" style={{overflow:"hidden"}}>
+                <hr className="ms-auto col-10 col-md-6" data-aos="fade-up" data-aos-delay="300" style={{padding: '0.1rem'}}/>
+                <div className="mx-auto col-10 col-md-6" data-aos="fade-up" data-aos-delay="400">
+                    <span className="d-flex justify-content-center display-2 m-0" style={{}}>
+                        Luxurious-
+                        <div className="d-flex">
+                            <span data-aos="fade-left" data-aos-delay="300"> B </span>
+                            <span data-aos="fade-left" data-aos-delay="350"> r </span>
+                            <span data-aos="fade-left" data-aos-delay="400"> o </span>
+                            <span data-aos="fade-left" data-aos-delay="450"> w </span>
+                            <span data-aos="fade-left" data-aos-delay="500"> n </span>
+                        </div>
+                    </span>
+                </div>
+                <hr className="col-10 col-md-6" data-aos="fade-up" data-aos-delay="600" style={{padding: '0.1rem'}}/>
+            </div>
             <div>
                 <div className="py-3 py-md-5">
-                    {/* <div className="mx-auto col-10 col-md-6" data-aos="fade-up" data-aos-delay="400">
-                        <p className="fs-4">
-                            Our goal is to achieve the sub par standard of living and making our home as beautiful 
-                            as our soul. We ensure that you get all the things that you ask for starting from your 
-                            wall paint till your furniture. We expertise in designing modern residents, commercial 
-                            sites and hospitality. 
-                        </p>
-                    </div> */}
+                <div className="container-fluid">
+                        <div className="row" >
+                            <div className="col-md-4 col-sm-6 col-12">
+                                {/* <div className="card text-white border-0 m-1 m-md-3">
+                                    <div className="card-body">
+                                        <h2 className="text-center display-3">Luxurious Brown</h2>
+                                    </div>
+                                </div> */}
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_10} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_1} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_3} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_11} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-12">
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_2} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_6} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_8} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-12">
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_7} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_9} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_4} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                                <div className="card text-white border-0 rounded-lg m-1 m-md-3">
+                                    <div className="shadow" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                                        <img className="card-img" src={proj_1_5} alt="Luxurious Brown"/>                                    </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div>
